06828210699949cd741d7eff6c3121e48e7e20b2
Kirill Gorobets <kgorob@gmail.com>
2018-01-10 02:07:54 +0300

    Two-directional motor control

--- two-wheel-robot.c
+++ two-wheel-robot.c
@@ -11,18 +11,33 @@ int main() {
 
 	rf24_pins_init();
 	pwm_0_init(PWM_PRESCALER_64);
+	pwm_1_init(PWM_PRESCALER_64);
 	spi_master_init(SPI_PRESCALER_16);
 	log_write("SPI initialized");
 
-	rf24_registers_init("\xab\x28\x38\xbe\xa7", "\xdb\x94\xe4\x10\x28", 2);
+	rf24_registers_init("\xab\x28\x38\xbe\xa7", "\xdb\x94\xe4\x10\x28", 3);
 	log_write("RF24 registers initialized");
 
 	while (1) {
-		rf24_receive(buffer, 2);
+		rf24_receive(buffer, 3);
 		uint8_t engine1 = buffer[0];
 		uint8_t engine2 = buffer[1];
-		pwm_pd5_write(engine1);
-		pwm_pd6_write(engine2);
-		log_write_format("RF24 request %d %d", engine1, engine2);
+		uint8_t directions = buffer[2];
+		if (!check_flag(directions, 0)) {
+			pwm_pd5_write(engine1);
+			pwm_pb1_write(0);
+		} else {
+			pwm_pd5_write(0);
+			pwm_pb1_write(engine1);
+		}
+		if (!check_flag(directions, 1)) {
+			pwm_pd6_write(engine2);
+			pwm_pb2_write(0);
+		} else {
+			pwm_pd6_write(0);
+			pwm_pb2_write(engine2);
+		}
+
+		log_write_format("RF24 request %d %d %02X", engine1, engine2, directions);
 	}
 }
